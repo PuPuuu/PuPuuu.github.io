5f89bc9e74fdf83969a5f9d833465d1e4720c3ac
1350562592@qq.com <jjye2020@163.com>
2023-10-07 18:54:35 +0800

    Site updated: 2023-10-07 18:54:35

--- js/txmap.js
+++ js/txmap.js
@@ -29,7 +29,7 @@ function getDistance(e1, n1, e2, n2) {
 
 function showWelcome() {
 
-    let dist = getDistance(104.065735, 30.659462, ipLoacation.result.location.lng, ipLoacation.result.location.lat); //这里换成自己的经纬度
+    let dist = getDistance(104.06573500, 30.65946200, ipLoacation.result.location.lng, ipLoacation.result.location.lat); //这里换成自己的经纬度
     let pos = ipLoacation.result.ad_info.nation;
     let ip;
     let posdesc;
@@ -225,4 +225,7 @@ function showWelcome() {
 }
 window.onload = showWelcome;
 // 如果使用了pjax在加上下面这行代码
-document.addEventListener('pjax:complete', showWelcome);
+// document.addEventListener('pjax:complete', showWelcome);
+
+
+
